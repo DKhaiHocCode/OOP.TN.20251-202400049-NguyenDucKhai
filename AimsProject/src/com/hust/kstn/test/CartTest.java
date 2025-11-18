f27f14798a72e0db7d44eb8afd3302ac77eccff2
Nguyen Duc Khai <237079454+DKhaiHocCode@users.noreply.github.com>
2025-11-18 16:33:15 +0700

    refactor: Add Aims.java and move Cart/DigitalVideoDisc to models package

--- AimsProject/src/com/hust/kstn/test/CartTest.java
+++ AimsProject/src/com/hust/kstn/test/CartTest.java
@@ -1,9 +1,9 @@
 package com.hust.kstn.test;
-import com.hust.kstn.Cart;
-import com.hust.kstn.DigitalVideoDisc;
+import com.hust.kstn.models.Cart;
+import com.hust.kstn.models.DigitalVideoDisc;
 
 public class CartTest {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Cart cart = new Cart();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95);
@@ -25,21 +25,21 @@ public static void main(String[] args) {
         System.out.println("\n TEST 4: Kiểm thử Giỏ hàng Đầy (Max 20) ");
         DigitalVideoDisc[] dvdsToFill = new DigitalVideoDisc[15];
         for (int i = 0; i < 15; i++) {
-            dvdsToFill[i] = new DigitalVideoDisc("DVD " + (i + 6)); 
+            dvdsToFill[i] = new DigitalVideoDisc("DVD " + (i + 6));
         }
-        cart.addDVD(dvdsToFill); 
-        cart.print(); 
+        cart.addDVD(dvdsToFill);
+        cart.print();
 
         DigitalVideoDisc dvdExtra = new DigitalVideoDisc("Extra DVD");
         cart.addDVD(dvdExtra);
         cart.print();
 
         System.out.println("\n TEST 5: Kiểm thử Xóa Sản phẩm Tồn tại ");
-        cart.removeDVD(dvd1); 
+        cart.removeDVD(dvd1);
         cart.print();
 
         System.out.println("\n TEST 6: Kiểm thử Xóa Sản phẩm KHÔNG Tồn tại ");
-        cart.removeDVD(dvdExtra); 
+        cart.removeDVD(dvdExtra);
         cart.print();
 
         System.out.println("\n TEST 7: Kiểm thử Xóa khi Giỏ hàng Rỗng ");
